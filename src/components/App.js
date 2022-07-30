cc5f7839094655a269e4f51388efa0c267522510
Alex Lewis <alexmichael.lewis@gmail.com>
2022-07-30 12:27:46 -0400

    feat(app): Initial commit, app creation

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Header from "./Header";
+
+import "../styles/App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
