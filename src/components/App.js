a9598083620d5b599dd78943e11d329dbde68fc1
Alex Lewis <alexmichael.lewis@gmail.com>
2022-07-30 21:00:45 -0400

    feat(context): App cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+
+import { TodoProvider } from "../contexts/todo-context";
 
 import Header from "./Header";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
+import ClearCompletedButton from "./ClearCompletedButton";
 
 import "../styles/App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const handleToggleCompleted = (id) => {
-    const updatedTodos = todos.map((todo) => {
-      return todo.id === id
-        ? { ...todo, completed: !todo.completed }
-        : { ...todo };
-    });
-    setTodos(updatedTodos);
-  };
-
-  const handleDeleteTodo = (id) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
-  };
-
-  const handleClearCompleted = () => {
-    const updatedTodos = todos.filter((todo) => !todo.completed);
-    setTodos(updatedTodos);
-  };
-
-  const handleAddTodo = (text) => {
-    const newTodo = {
-      id: Date.now(),
-      text,
-      completed: false,
-    };
-    setTodos([...todos, newTodo]);
-  };
-
   return (
     <div className="App container">
       <Header />
-      <TodoList
-        todoList={todos}
-        handleDeleteTodo={handleDeleteTodo}
-        handleToggleCompleted={handleToggleCompleted}
-        handleClearCompleted={handleClearCompleted}
-      />
-      <TodoForm handleAddTodo={handleAddTodo} />
-      {todos.length > 0 ? (
-        <button className="mt-1 button is-danger" onClick={handleClearCompleted}>
-          Clear Completed
-        </button>
-      ) : null}
+      <TodoProvider>
+        <TodoList />
+        <TodoForm />
+        <ClearCompletedButton />
+      </TodoProvider>
     </div>
   );
 }
